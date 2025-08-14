bad802b01bc2c3fe8893dbf33e75eb72f1558b7f
jaabless <jefferyasamoahbless@gmail.com>
2025-08-14 09:01:34 +0000

    Add Endpoint class to handle all API endpoints

---
+++ src/test/java/com/example/fakestoreapi/config/Endpoints.java
@@ -0,0 +1,13 @@
+package com.example.fakestoreapi.config;
+
+
+public class Endpoints {
+    public static final String PRODUCTS = "/products";
+    public static final String PRODUCTS_DETAILS = "/products/{id}";
+    public static final String USERS = "/users";
+    public static final String USERS_DETAILS = "/users/{id}";
+    public static final String CARTS = "/carts";
+    public static final String CARTS_DETAILS = "/carts/{id}";
+    public static final String AUTH = "/auth/login";
+}
+
